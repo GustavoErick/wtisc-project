7381639216a7732a6c4823b951df434da150c9bc
Gustavo Erick <gustavoerickv@gmail.com>
2024-05-14 21:22:41 -0300

    removendo comentarios desnecessarios de app.js

--- api/app.js
+++ api/app.js
@@ -1,8 +1,5 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-// import ejs from 'ejs';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
 
 import userRoute from './routes/user.route.js';
 import authRoute from './routes/auth.route.js';
@@ -19,51 +16,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(cookieParser());
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// app.get('/certificado', (req, res) => {
-//     const filePath = path.join(__dirname, './certificado.ejs');
-//     ejs.renderFile(filePath, (err, html) => {
-//         if (err) {
-//             console.log(err);
-//             return res.send('Erro na leitura do arquivo');
-//         }
-    
-//         // enviar para o navegador
-//         return res.send(html);
-//     });
-// });
-
-// app.get('/pdf', async (req, res) => {
-//     // inicia navegador
-//     const browser = await puppeteer.launch();
-//     // navegador abre uma nova página
-//     const page = await browser.newPage();
-
-//     const urlSite = 'http://localhost:3333/certificado';
-
-//     await page.goto(urlSite, {
-//         waitUntil: 'networkidle0'
-//     });
-
-//     await page.emulateMediaType('print');
-
-//     const pdf = await page.pdf({
-//         printBackground: true,
-//         format: 'A4',
-//         landscape: true
-        
-//     });
-
-//     await browser.close();
-
-//     res.contentType('application/pdf');
-
-//     res.send(pdf);
-
-// });
 
 // ROTAS
 app.use('/users', userRoute);
