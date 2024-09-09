4831f4d4c14d4afdefd0fddf626d7f748b67cc1a
beatriznnascimento <beatriznascimento@alu.ufc.br>
2024-09-08 22:42:08 -0300

    test: tests for authentication

---
+++ api/jest-config.js
@@ -0,0 +1,11 @@
+export default {
+    roots: [
+        "<rootDir>/test"
+    ],
+    testRegex: 'test/(.+)\\.test\\.(jsx?|tsx?)$',
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
